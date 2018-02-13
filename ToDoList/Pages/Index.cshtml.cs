e146756466b97ee00a7b72669dca13e863a5aec7
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-02-12 19:44:01 -0800

    Moves To-Do List pages to root of website, replacing boilerplate pages

--- ToDoList/Pages/Index.cshtml.cs
+++ ToDoList/Pages/Index.cshtml.cs
@@ -1,17 +1,46 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
-namespace ToDoList.Pages
+namespace ToDoList.Pages.Entries
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly EntryContext _context;
 
+        public IndexModel(EntryContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EntryItem> Entries { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var entryList = await _context.Entry.ToListAsync();
+
+            Entries = new List<EntryItem>();
+
+            foreach (var entry in entryList)
+            {
+                if (entry.Parent == 0)
+                {
+                    Entries.Add(new EntryItem(entry));
+                }
+                else
+                {
+                    foreach (var entryItem in this.Entries)
+                    {
+                        var entryListItem = Entry.FindParentForEntry(entry, entryItem);
+                        if (entryListItem.Entry == null) continue;
+
+                        entryListItem.Children.Add(new EntryItem(entry));
+                        break;
+                    }
+                }
+            }
         }
     }
 }
