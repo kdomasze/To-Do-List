4074d54064b1f03a30c6aac76c81cc678621ff53
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-02-15 17:50:07 -0800

    Adds viewComponent for displaying tasks

---
+++ ToDoList/ViewComponents/TaskItemViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.ViewComponents
+{
+    public class TaskItemViewComponent : ViewComponent
+    {
+        public TaskItemViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(TaskItem taskitem)
+        {
+            return View(taskitem.Task);
+        }
+    }
+}
